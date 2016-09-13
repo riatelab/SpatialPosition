478bdbebdc830f7cda1a6b3a95eac26af2809269
Timothée Giraud <timothee.giraud@ums-riate.fr>
2016-09-13 14:42:30 +0200

    doc, clean, example

--- R/par.R
+++ R/par.R
@@ -1,7 +1,7 @@
 #' @title Stewart Potentials Parallel
-#' @name stewartParallel
-#' @description This function computes the potentials with parallel computing. 
-#' EXPERIMENTAL use. Only Great Circle Distances are used. 
+#' @name parStewart
+#' @description This function computes steawart potentials using parallel 
+#' computation. 
 #' @param knownpts sp object (SpatialPointsDataFrame or SpatialPolygonsDataFrame);
 #' this is the set of known observations to estimate the potentials from.
 #' @param unknownpts sp object (SpatialPointsDataFrame or SpatialPolygonsDataFrame); 
@@ -24,81 +24,108 @@
 #'  points. (optional)
 #' @param mask sp object; the spatial extent of this object is used to 
 #' create the regularly spaced SpatialPointsDataFrame output. (optional)
-#' @param cl number of clusters
-#' @param chunks chunks size of unknowpts
-#' @return SpatialPointsDataFrame with the computed potentials in a new field 
-#' named \code{OUTPUT}
-#' @seealso \link{rasterStewart}, \link{plotStewart}, \link{quickStewart},
-#' \link{rasterToContourPoly}, \link{CreateGrid}, \link{CreateDistMatrix}.
+#' @param cl numeric; number of clusters. By default cl is determined using 
+#' \code{parallel::detectCores()}.
+#' @param size numeric; parStewart split unknownpts in chunks, size indicated the 
+#' size of each chunks.  
+#' @details The parallel implementation splits potentials computations along 
+#' chunks of unknownpts (or chunks of the grid defined using resolution). It only 
+#' uses Great Cercle distances (with \code{\link{CreateDistMatrix}}). 
+#' @return A Spatial*DataFrame with the computed potentials in a new field 
+#' named \code{OUTPUT} is returned. 
+#' @seealso \link{stewart}.
 #' @examples 
 #' \dontrun{
 #' if(require(cartography)){
-#' system.time(
-#'   s1 <- stewart(knownpts = nuts3.spdf,resolution = 40000, 
-#'                varname = "pop2008",
-#'                typefct = "exponential", span = 100000,
-#'                beta = 3, mask = nuts3.spdf)
-#' )
-#' 
-#' system.time(
-#'   s2 <- stewartParallel(knownpts = nuts3.spdf, resolution = 40000, 
-#'                         varname = "pop2008",
-#'                         typefct = "exponential", span = 100000,
-#'                         beta = 3, mask = nuts3.spdf, cl = 4, chunks = 500)
-#' )
-#' 
-#' identical(s1, s2)
-#' 
-#' r2 <- rasterStewart(s2)
-#' c2 <- rasterToContourPoly(r2, breaks = c(0,10000,100000,1000000,2004000,
-#'                                          5000000,10040000, 20000000200000000), 
-#'                           mask = nuts3.spdf)
-#' 
-#'  par(mar = c(0,0,0,0))
-#'  bks <- sort(unique(c(c2$min, c2$max)))
-#'  choroLayer(spdf = c2,
-#'             var = "center", legend.pos = "topleft",
-#'             breaks = bks, border = NA)
+#'   nuts3.spdf@data <- nuts3.df
+#'   t1 <- system.time(
+#'     s1 <- stewart(knownpts = nuts3.spdf,resolution = 40000, 
+#'                   varname = "pop2008",
+#'                   typefct = "exponential", span = 100000,
+#'                   beta = 3, mask = nuts3.spdf)
+#'   )
+#'   
+#'   t2 <- system.time(
+#'     s2 <- parStewart(knownpts = nuts3.spdf, resolution = 40000, 
+#'                      varname = "pop2008",
+#'                      typefct = "exponential", span = 100000,
+#'                      beta = 3, mask = nuts3.spdf, cl = 3, size = 500)
+#'   )
+#'   identical(s1, s2)
+#'   cat("Elapsed time\n", "stewart:", t1[3], "\n parStewart:",t2[3])
+#'   
+#'   r2 <- rasterStewart(s2)
+#'   c2 <- rasterToContourPoly(r = r2, breaks = c(0,1000000,2000000, 5000000,
+#'                                                10000000, 20000000, 200004342), 
+#'                             mask = nuts3.spdf)
+#'   # cartography
+#'   opar <- par(mar = c(0,0,1.2,0))
+#'   bks <- sort(unique(c(c2$min, c2$max)))
+#'   choroLayer(spdf = c2, var = "center", breaks = bks, border = NA, 
+#'              legend.title.txt = "pop")
+#'   layoutLayer("potential population", "","", scale = NULL)
+#'   par(opar)
 #' }
 #' }
-#' @references 
-#' STEWART J.Q. (1942) "Measure of the influence of a population at a distance", Sociometry, 5(1): 63-71.  
 #' @import sp
 #' @import raster
 #' @import foreach
-#' @import doParallel
 #' @export
-stewartParallel <- function(knownpts, unknownpts = NULL,
-                            varname,
-                            typefct = "exponential",
-                            span, beta, resolution = NULL,
-                            mask = NULL, cl = 2, chunks = 100){
+parStewart <- function(knownpts, unknownpts = NULL,
+                       varname,
+                       typefct = "exponential",
+                       span, beta, resolution = NULL,
+                       mask = NULL, cl = 2, size = 100){
   
+  # if (!requireNamespace("parallel", quietly = TRUE)) {
+  #   stop("'foreach' package needed for this function to work. Please install it.",
+  #        call. = FALSE)
+  # }
+  # if (!requireNamespace("foreach", quietly = TRUE)) {
+  #   stop("'foreach' package needed for this function to work. Please install it.",
+  #        call. = FALSE)
+  # }
+  # if (!requireNamespace("doParallel", quietly = TRUE)) {
+  #   stop("'foreach' package needed for this function to work. Please install it.",
+  #        call. = FALSE)
+  # }
+  # 
+  # print(search)
+  # if(!'package:doParallel' %in% search()){
+  #   attachNamespace('doParallel')
+  # }
+  # if(!'package:foreach' %in% search()){
+  #   attachNamespace('foreach')
+  # }
+  # print(search)
   
+
   if (is.null(unknownpts)){
     unknownpts <- CreateGrid(w = if(is.null(mask)){knownpts} else {mask},
                              resolution = resolution)
     unknownpts2 <- unknownpts
   }else{
+    # SpatialPolygons to SpatialPoints
     if(methods::is(object = unknownpts, "SpatialPolygons")){
       unknownpts2 <- SpatialPointsDataFrame(coordinates(unknownpts),
                                             data = unknownpts@data,
                                             proj4string = unknownpts@proj4string)
     }
   }
-  
+  # SpatialPolygons to SpatialPoints
   if(methods::is(object = knownpts, "SpatialPolygons")){
     knownpts <- SpatialPointsDataFrame(coordinates(knownpts),
                                        data = knownpts@data,
                                        proj4string = knownpts@proj4string)
   }
   
-  
+  # force wgs84
   if(sp::is.projected(knownpts)){
     knownpts <- sp::spTransform(knownpts,"+init=epsg:4326")
     unknownpts2 <- sp::spTransform(unknownpts2,"+init=epsg:4326")
   }
   
+  # launch multiple cores
   if (is.null(cl)){
     cl <- parallel::detectCores(all.tests = FALSE, logical = FALSE)
   }
@@ -106,31 +133,28 @@ stewartParallel <- function(knownpts, unknownpts = NULL,
   doParallel::registerDoParallel(cl)
   
   
-  # sequence pour découper la grille
-  sequence <- unique(c(seq(1,nrow(unknownpts2), chunks),nrow(unknownpts2)+1) )
-  
-  # nombre d'itération
+  # sequence to split unknowpts
+  sequence <- unique(c(seq(1,nrow(unknownpts2), size),nrow(unknownpts2)+1))
   lseq <- length(sequence)-1
   
-  # cut the unknownpts
+  # split unknownpts and put it on a list
   ml <- list()
   for  (i in 1:lseq){
-    # cat(sequence[i], "-",sequence[i+1]-1, "\n")
     ml[[i]] <- unknownpts2[(sequence[i]):(sequence[i+1]-1),]
   }
-  
-  ls <- foreach(i = ml, .packages = c('SpatialPosition'),
-                .combine = rbind, .inorder = TRUE) %dopar% {
-                  mat <- CreateDistMatrix(knownpts = knownpts,
-                                          unknownpts = i,
-                                          bypassctrl = TRUE)
-                  st <- stewart(knownpts = knownpts,
-                                unknownpts = i,
-                                matdist = mat,
-                                typefct = typefct,
-                                span = span, beta = beta,
-                                varname = varname)
-                }
+
+  ls <- foreach::foreach(i = ml, .packages = c('SpatialPosition'),
+                         .combine = rbind, .inorder = TRUE) %dopar% {
+                           mat <- CreateDistMatrix(knownpts = knownpts,
+                                                   unknownpts = i,
+                                                   bypassctrl = TRUE)
+                           st <- stewart(knownpts = knownpts,
+                                         unknownpts = i,
+                                         matdist = mat,
+                                         typefct = typefct,
+                                         span = span, beta = beta,
+                                         varname = varname)
+                         }
   
   parallel::stopCluster(cl)
   unknownpts$OUTPUT <- ls$OUTPUT
